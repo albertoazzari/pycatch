baa6bdfad1978c9dd02d7de3a3bd47af301252fb
Alberto <alberto.azzari@univr.it>
2024-12-09 18:11:48 +0100

    name change from pycatch22 to pycatch

--- pycatch/__init__.py
+++ pycatch/__init__.py
@@ -1,11 +1,11 @@
-from pycatch22 import pycatch22
+from pycatch import pycatch
 from typeguard import typechecked
 import numpy as np
 
 @typechecked
 def compute(x: np.ndarray, n:int) -> float:
-    return pycatch22.compute(x, n)
+    return pycatch.compute(x, n)
 
 @typechecked
 def zscore(x: np.ndarray) -> np.ndarray:
-    return pycatch22.zscore(x)
+    return pycatch.zscore(x)
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod catch22;
 pub const N_CATCH22: usize = 25;
 
 #[pymodule]
-#[pyo3(name = "cd_diagram")]
+#[pyo3(name = "pycatch")]
 fn py_module(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(catch22::compute, m)?)?;
     m.add_function(wrap_pyfunction!(catch22::zscore, m)?)?;
